53c03f295c72ac17d1d5e43869174e689b8e84a7
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2023-05-18 12:29:55 -0400

    Added variable arity

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,28 @@ macro_rules! add {
     }};
 }
 
+// use macro_rule! <name of macro> { <Body> }
+macro_rules! add_as {
+    // using a ty token type for matching datatypes passed to macro
+    // ($a:expr,$b:expr,$typ:ty) => {
+    //     $a as $typ + $b as $typ
+    // };
+
+    ($typ:ty, $($a:expr),*) => {
+        {
+            let mut sum = 0;
+            $(
+                sum += $a as $typ;
+            )*
+            sum
+        }
+    }
+}
+
 fn main() {
     // println!("{}", add!(1, 2));
     let x = 0;
     println!("{}", add!(1, 2));
     println!("{}", add!(x));
+    println!("{}", add_as!(u8, 0, 2, 3));
 }
